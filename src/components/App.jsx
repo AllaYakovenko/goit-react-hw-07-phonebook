1c5a255f334f99ff775eedfc7e2c08f9def25269
AllaYakovenko <jaak280488@gmail.com>
2023-03-20 19:19:51 +0200

    hw-07

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,27 @@
+import css from 'components/App.module.css';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { selectError, selectIsLoading } from '../redux/selectors/selectors';
+import { ThreeDots } from 'react-loader-spinner';
+import { fetchContacts } from "../redux/operations";
+
+
 export const App = () => {
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -10,7 +29,13 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <div>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm />
+        <h2 className={css.title}>Contacts</h2>
+        <Filter />
+        {isLoading&&!error?<div className={css.loader}><ThreeDots /></div>:<ContactList /> }
+      </div>
     </div>
   );
 };
